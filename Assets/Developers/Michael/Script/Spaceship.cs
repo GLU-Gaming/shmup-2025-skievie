b8f8ae38a687fe01f4e366f886ed1a27d5ae8716
NyckdeVries21 <michaelwitkamp16@gmail.com>
2025-03-20 09:11:53 +0100

    20-3-2025 Dev kant build

---
+++ Assets/Developers/Michael/Script/Spaceship.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] public float moveSpeed = 2;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+}
