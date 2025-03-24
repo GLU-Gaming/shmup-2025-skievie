703d22ebdf0440788da2aa747124995de0139f6a
NyckdeVries21 <michaelwitkamp16@gmail.com>
2025-03-24 12:01:37 +0100

    start van de list met enemies spawnen

--- Assets/Developers/Script/GameManagement.cs
+++ Assets/Developers/Script/GameManagement.cs
@@ -4,10 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class GameManagement : MonoBehaviour
 {
-    [SerializeField] private GameObject Enemies;
+    [SerializeField] private GameObject[] Enemies;
     [SerializeField] private float EnemyAmount = 4;
 
     [SerializeField] private List<GameObject> spawnedEnemies = new List<GameObject>();
+
+    [SerializeField] private LayerMask enemyLayer;
 
     public float lifeAmount = 3;
 
@@ -24,13 +26,46 @@ public class GameManagement : MonoBehaviour
         
     }
 
+    private void StartNewRound()
+    {
+        if (spawnedEnemies.Count < EnemyAmount)
+        {
+            Vector3 spawnpoint = new Vector3(34, Random.Range(-15, 15), 0);
+
+            bool temp = AsteroidPlayerOverlap(spawnpoint, 1);
+            if (temp)
+            {
+                GameObject go = Instantiate(GameObject[Enemies], spawnpoint, transform.rotation);
+                spawnedEnemies.Add(go);
+            }
+        }
+    }
+
     public void RemoveEnemy(GameObject enemiesToRemove) // verwijderen van enemy
     {
         
         spawnedEnemies.Remove(enemiesToRemove);
         Destroy(enemiesToRemove);
 
-        
+        if (spawnedEnemies.Count == 0)
+        {
+            StartNewRound();
+        }
+
+    }
+
+    public bool AsteroidPlayerOverlap(Vector3 center, float radius)
+    {
+        bool freeSpace = false;
+
+        Collider[] Asteroids = Physics.OverlapSphere(Vector3.zero, radius, enemyLayer.value);
+        if (Asteroids.Length == 0)
+        {
+            freeSpace = true;
+        }
+        return freeSpace;
+
+
 
     }
 
