742764bc6e25ed7d12f1f03499e166b9578ba346
NyckdeVries21 <michaelwitkamp16@gmail.com>
2025-04-08 10:33:09 +0200

    Update MiddleEnemy.cs

--- Assets/Developers/Script/EnemyScript/MiddleEnemy.cs
+++ Assets/Developers/Script/EnemyScript/MiddleEnemy.cs
@@ -4,7 +4,7 @@ public class MiddleEnemy : EnemyScript
 {
     public override void Activate()
     {
-        transform.rotation = Quaternion.Euler(-90, 0, 90);
+        transform.rotation = Quaternion.Euler(0, 90, 0);
         base.Activate();
         fireRate = 0.75f;
         HPamount = 9;
